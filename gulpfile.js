8040d9eb56837c0c2a87888594f5582f6e84714b
Philipp <philipp_ten_brink@web.de>
2018-02-07 22:37:34 +0100

    Core changes to fit Verium

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 
 
-var bitcoreTasks = require('bitcore-build');
+var veriumcoreTasks = require('veriumcore-build');
 
-bitcoreTasks('lib');
+veriumcoreTasks('lib');
--- index.js
+++ index.js
@@ -1,69 +1,69 @@
 'use strict';
 
-var bitcore = module.exports;
+var veriumcore = module.exports;
 
 // module information
-bitcore.version = 'v' + require('./package.json').version;
-bitcore.versionGuard = function(version) {
+veriumcore.version = 'v' + require('./package.json').version;
+veriumcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib found. ' +
-      'Please make sure to require bitcore-lib and check that submodules do' +
-      ' not also include their own bitcore-lib dependency.';
+    var message = 'More than one instance of veriumcore-lib found. ' +
+      'Please make sure to require veriumcore-lib and check that submodules do' +
+      ' not also include their own veriumcore-lib dependency.';
     throw new Error(message);
   }
 };
-bitcore.versionGuard(global._bitcore);
-global._bitcore = bitcore.version;
+veriumcore.versionGuard(global._veriumcore);
+global._veriumcore = veriumcore.version;
 
 // crypto
-bitcore.crypto = {};
-bitcore.crypto.BN = require('./lib/crypto/bn');
-bitcore.crypto.ECDSA = require('./lib/crypto/ecdsa');
-bitcore.crypto.Hash = require('./lib/crypto/hash');
-bitcore.crypto.Random = require('./lib/crypto/random');
-bitcore.crypto.Point = require('./lib/crypto/point');
-bitcore.crypto.Signature = require('./lib/crypto/signature');
+veriumcore.crypto = {};
+veriumcore.crypto.BN = require('./lib/crypto/bn');
+veriumcore.crypto.ECDSA = require('./lib/crypto/ecdsa');
+veriumcore.crypto.Hash = require('./lib/crypto/hash');
+veriumcore.crypto.Random = require('./lib/crypto/random');
+veriumcore.crypto.Point = require('./lib/crypto/point');
+veriumcore.crypto.Signature = require('./lib/crypto/signature');
 
 // encoding
-bitcore.encoding = {};
-bitcore.encoding.Base58 = require('./lib/encoding/base58');
-bitcore.encoding.Base58Check = require('./lib/encoding/base58check');
-bitcore.encoding.BufferReader = require('./lib/encoding/bufferreader');
-bitcore.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
-bitcore.encoding.Varint = require('./lib/encoding/varint');
+veriumcore.encoding = {};
+veriumcore.encoding.Base58 = require('./lib/encoding/base58');
+veriumcore.encoding.Base58Check = require('./lib/encoding/base58check');
+veriumcore.encoding.BufferReader = require('./lib/encoding/bufferreader');
+veriumcore.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
+veriumcore.encoding.Varint = require('./lib/encoding/varint');
 
 // utilities
-bitcore.util = {};
-bitcore.util.buffer = require('./lib/util/buffer');
-bitcore.util.js = require('./lib/util/js');
-bitcore.util.preconditions = require('./lib/util/preconditions');
+veriumcore.util = {};
+veriumcore.util.buffer = require('./lib/util/buffer');
+veriumcore.util.js = require('./lib/util/js');
+veriumcore.util.preconditions = require('./lib/util/preconditions');
 
 // errors thrown by the library
-bitcore.errors = require('./lib/errors');
+veriumcore.errors = require('./lib/errors');
 
 // main bitcoin library
-bitcore.Address = require('./lib/address');
-bitcore.Block = require('./lib/block');
-bitcore.MerkleBlock = require('./lib/block/merkleblock');
-bitcore.BlockHeader = require('./lib/block/blockheader');
-bitcore.HDPrivateKey = require('./lib/hdprivatekey.js');
-bitcore.HDPublicKey = require('./lib/hdpublickey.js');
-bitcore.Networks = require('./lib/networks');
-bitcore.Opcode = require('./lib/opcode');
-bitcore.PrivateKey = require('./lib/privatekey');
-bitcore.PublicKey = require('./lib/publickey');
-bitcore.Script = require('./lib/script');
-bitcore.Transaction = require('./lib/transaction');
-bitcore.URI = require('./lib/uri');
-bitcore.Unit = require('./lib/unit');
+veriumcore.Address = require('./lib/address');
+veriumcore.Block = require('./lib/block');
+veriumcore.MerkleBlock = require('./lib/block/merkleblock');
+veriumcore.BlockHeader = require('./lib/block/blockheader');
+veriumcore.HDPrivateKey = require('./lib/hdprivatekey.js');
+veriumcore.HDPublicKey = require('./lib/hdpublickey.js');
+veriumcore.Networks = require('./lib/networks');
+veriumcore.Opcode = require('./lib/opcode');
+veriumcore.PrivateKey = require('./lib/privatekey');
+veriumcore.PublicKey = require('./lib/publickey');
+veriumcore.Script = require('./lib/script');
+veriumcore.Transaction = require('./lib/transaction');
+veriumcore.URI = require('./lib/uri');
+veriumcore.Unit = require('./lib/unit');
 
 // dependencies, subject to change
-bitcore.deps = {};
-bitcore.deps.bnjs = require('bn.js');
-bitcore.deps.bs58 = require('bs58');
-bitcore.deps.Buffer = Buffer;
-bitcore.deps.elliptic = require('elliptic');
-bitcore.deps._ = require('lodash');
+veriumcore.deps = {};
+veriumcore.deps.bnjs = require('bn.js');
+veriumcore.deps.bs58 = require('bs58');
+veriumcore.deps.Buffer = Buffer;
+veriumcore.deps.elliptic = require('elliptic');
+veriumcore.deps._ = require('lodash');
 
 // Internal usage, exposed for testing/advanced tweaking
-bitcore.Transaction.sighash = require('./lib/transaction/sighash');
+veriumcore.Transaction.sighash = require('./lib/transaction/sighash');
